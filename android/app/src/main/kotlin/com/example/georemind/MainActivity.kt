0707dbc0bbfa893569fdd946b90b4f563e9b88d8
BuddhimaB <balasooriya.21@uom.lk>
2025-12-02 00:55:03 +0530

    Initialize GeoRemind Flutter project

---
+++ android/app/src/main/kotlin/com/example/georemind/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.georemind
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:geo_remind/main.dart';
+import 'package:georemind/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
